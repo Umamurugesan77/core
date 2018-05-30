665077e72cda1e4fd2f45cc1b13f8118a7837c12
Umamurugesan77 <umadurka1998@gmail.com>
2018-05-30 03:52:04 -0700

    dor4ex

---
+++ dor4.java
@@ -0,0 +1,19 @@
+public class dor4
+{
+public static void main( String[] args )
+{
+int y;
+int x = 1;
+int total = 0;
+
+ while ( x <= 10 )
+ {
+ y = x * x;
+ System.out.println( y );
+ total += y;
+ ++x;
+System.out.println(total);
+ } 
+}
+}
+
