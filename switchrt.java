fca944b7940ae661d097c2c911b310757b08d1ec
Umamurugesan77 <umadurka1998@gmail.com>
2018-05-30 02:37:21 -0700

    switchrt

---
+++ switchrt.java
@@ -0,0 +1,18 @@
+public class switchrt{
+public static void main(String args[])
+{
+int choice = 5;
+switch(choice)
+{
+case 5:
+    System.out.println(5);
+    break;
+case 10:
+   System.out.println(10);
+   break;
+default:
+  System.out.println(0);
+break;
+}
+}
+}
