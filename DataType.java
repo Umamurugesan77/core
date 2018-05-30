f595d047b2144bd0004367c33ad5c4a7ac81bfd5
Umamurugesan77 <umadurka1998@gmail.com>
2018-05-30 01:51:40 -0700

    DataType

---
+++ DataType.java
@@ -0,0 +1,17 @@
+package com.basics;
+
+public class DataType {
+	public static void main(String args[])
+	{
+	int count = 5;
+	if(count < 6)
+	{
+		System.out.println(6);
+	}else if(count == 5)
+	{
+		System.out.println(5);
+	}
+}
+}
+
+
