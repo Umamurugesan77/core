dd9733746d37eb193a85f7d281b8e33bf34df7d4
Umamurugesan77 <umadurka1998@gmail.com>
2018-06-05 02:07:13 -0700

    typecast2

---
+++ TypeCast2.java
@@ -0,0 +1,12 @@
+public class typecast2 {
+	public static void main(String a[]) {
+		byte b1=3;
+		short s1=130;
+		s1=b1;
+		b1=(byte)s1;
+		System.out.println(s1);
+		System.out.println(b1);
+	}
+
+}
+
